45f0dc5e71a1c495ed1063a63894c5237a1ecd4a
TravisAdsitt <70sRocker@gmail.com>
2023-01-29 11:01:35 -0700

    Added file prefix to arguments

--- src/spelling_maze.cpp
+++ src/spelling_maze.cpp
@@ -2,10 +2,10 @@
 #include <pybind11/pybind11.h>
 
 
-void generate_maze(std::string word, int grid_width, int grid_height, int block_width = 20, int block_height = 20){
+void generate_maze(std::string word, int grid_width, int grid_height, std::string file_prefix, int block_width = 20, int block_height = 20){
     generator.seed(time(0));
     WordMaze m(word, grid_width, grid_height, block_width, block_height);
-    m.save_to_png(word + ".png");
+    m.save_to_png(file_prefix + word + ".png");
 }
 
 PYBIND11_MODULE(SpellingMaze, m) {
